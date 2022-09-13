b749e57139889780fd8c14a4bd4742870fc7c890
Dan Grossen <grossen@Dans-MacBook-Pro.local>
2022-09-13 14:14:20 -0600

    add edgeHitProcessed to types and index

--- packages/edge/src/index.ts
+++ packages/edge/src/index.ts
@@ -3,6 +3,7 @@
 export { default as analyticsHit } from './analyticsHit';
 export { default as analyticsMapping } from './analyticsMapping';
 export { default as edgeEvent } from './edgeEvent';
+export { default as edgeHitProcessed } from './edgeHitProcessed';
 export { default as edgeHitReceived } from './edgeHitReceived';
 export { default as streamingValidation } from './streamingValidation';
 export * from './types';
